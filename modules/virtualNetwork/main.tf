b017d744ac6d86efc7c0d02cb48fa28098e13076
Pikula <tomasz.pikula@accenture.com>
2025-02-03 16:42:11 +0100

    add dynamic vnets

--- modules/virtualNetwork/main.tf
+++ modules/virtualNetwork/main.tf
@@ -1,30 +1,25 @@
 module "vnet" {
-  source              = "git::https://github.com/az-lz-20-mb/mod-avm-res-virtualnetwork.git"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  name                = var.vnet_name
+  for_each = var.vnets
 
-  address_space = var.vnet_address_space
+  source              = "git::https://github.com/az-lz-20-mb/mod-avm-res-virtualnetwork.git"
+  resource_group_name = each.value.resource_group_name
+  location            = each.value.location
+  name                = each.value.vnet_name
+
+  address_space = each.value.vnet_address_space
 
   encryption = {
-    enabled = var.enable_encryption
+    enabled     = each.value.enable_encryption
     enforcement = "AllowUnencrypted"
   }
 
-  subnets = {
-    subnet1 = {
-      name                            = "${module.vnet.name}-subnet-1"
-      default_outbound_access_enabled = var.default_outbound_access_enabled
-      address_prefixes                = var.subnet1_address_prefixes
-    }
-    subnet2 = {
-      name                            = "${module.vnet.name}-subnet-2"
-      address_prefixes                = var.subnet2_address_prefixes
-      default_outbound_access_enabled = var.default_outbound_access_enabled
-    }
-  }
+  subnets = { for subnet_key, subnet_config in each.value.subnets : subnet_key => {
+    name                            = "${each.value.vnet_name}-${subnet_key}"
+    address_prefixes                = subnet_config.address_prefixes
+    default_outbound_access_enabled = subnet_config.default_outbound_access_enabled
+  }}
 }
 
-output "vnet_id" {
-  value = module.vnet.resource_id
+output "vnet_ids" {
+  value = { for k, v in module.vnet : k => v.resource_id }
 }
--- modules/virtualNetwork/variables.tf
+++ modules/virtualNetwork/variables.tf
@@ -1,41 +1,14 @@
-variable "vnet_name" {
-  description = "The name of the virtual network."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
-
-variable "location" {
-  description = "The location of the resource group."
-  type        = string
-}
-
-variable "vnet_address_space" {
-  description = "The address space of the virtual network."
-  type        = list(string)
-}
-
-variable "subnet1_address_prefixes" {
-  description = "The address prefixes of the subnet."
-  type        = list(string)
-}
-
-variable "subnet2_address_prefixes" {
-  description = "The address prefixes of the subnet."
-  type        = list(string)
-}
-
-variable "enable_encryption" {
-  description = "Enable encryption."
-  type        = bool
-  default     = true
-}
-
-variable "default_outbound_access_enabled" {
-  description = "Enable default outbound access."
-  type        = bool
-  default     = true
+variable "vnets" {
+  description = "The virtual networks."
+  type        = map(object({
+    resource_group_name = string
+    location            = string
+    vnet_name           = string
+    vnet_address_space  = list(string)
+    enable_encryption    = bool
+    subnets             = map(object({
+      address_prefixes                = list(string)
+      default_outbound_access_enabled = bool
+    }))
+  }))
 }
