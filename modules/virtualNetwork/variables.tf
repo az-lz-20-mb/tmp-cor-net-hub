797651b54c3ce5650ee7721a1c37f47e9f64d1d9
Pikula <tomasz.pikula@accenture.com>
2025-02-12 14:23:02 +0100

    refactor vnets

--- modules/virtualNetwork/variables.tf
+++ modules/virtualNetwork/variables.tf
@@ -1,9 +1,6 @@
 variable "vnets" {
   description = "The virtual networks."
   type        = map(object({
-    resource_group_name = string
-    location            = string
-    vnet_name           = string
     vnet_address_space  = list(string)
     enable_encryption    = bool
     subnets             = map(object({
