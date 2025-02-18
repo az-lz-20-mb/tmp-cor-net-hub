e43edf7d163ff65f69925c7ae2e194656d55f093
Pikula <tomasz.pikula@accenture.com>
2025-02-18 16:22:35 +0100

    refactor hub arch

---
+++ modules/virtualNetworkHub/main.tf
@@ -0,0 +1,45 @@
+module "hub_mesh" {
+  source = "git::https://github.com/az-lz-20-mb/mod-avm-ptn-hubnetworking.git"
+
+  hub_virtual_networks = {
+    for key, hub in var.hub_virtual_networks : key => {
+      name                            = hub.name
+      address_space                   = hub.address_space
+      location                        = hub.location
+      resource_group_name             = azurerm_resource_group.hub_rg[key].name
+      resource_group_creation_enabled = false
+      resource_group_lock_enabled     = false
+      mesh_peering_enabled            = true
+      route_table_name                = hub.route_table_name
+      routing_address_space           = hub.routing_address_space
+
+      firewall = hub.firewall != null ? {
+        subnet_address_prefix = hub.firewall.subnet_address_prefix
+        name                  = hub.firewall.name
+        sku_name              = hub.firewall.sku_name
+        sku_tier              = hub.firewall.sku_tier
+        zones                 = hub.firewall.zones
+        default_ip_configuration = {
+          public_ip_config = {
+            name  = hub.firewall.default_ip_configuration.public_ip_config.name
+            zones = hub.firewall.default_ip_configuration.public_ip_config.zones
+          }
+        }
+        firewall_policy = {
+          name = hub.firewall.firewall_policy.name
+          dns = {
+            proxy_enabled = hub.firewall.firewall_policy.dns.proxy_enabled
+          }
+        }
+      } : null
+
+      subnets = {
+        for subnet_key, subnet in hub.subnets : subnet_key => {
+          name             = subnet.name
+          address_prefixes = subnet.address_prefixes
+          route_table      = subnet.route_table
+        }
+      }
+    }
+  }
+}
---
+++ modules/virtualNetworkHub/variables.tf
@@ -0,0 +1,42 @@
+variable "hub_virtual_networks" {
+  type = map(object({
+    name            = string
+    address_space   = list(string)
+    location        = string
+    route_table_name = string
+    routing_address_space = list(string)
+
+    firewall = optional(object({
+      subnet_address_prefix = string
+      name                  = string
+      sku_name              = string
+      sku_tier              = string
+      zones                 = list(string)
+      default_ip_configuration = object({
+        public_ip_config = object({
+          name  = string
+          zones = list(string)
+        })
+      })
+      firewall_policy = object({
+        name = string
+        dns = object({
+          proxy_enabled = bool
+        })
+      })
+    }))
+
+    subnets = map(object({
+      name             = string
+      address_prefixes = list(string)
+      route_table      = optional(object({
+        assign_generated_route_table = bool
+      }))
+    }))
+  }))
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the virtual network will be created"
+  type        = string
+}
