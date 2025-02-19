a2496faf808488f76959d567292264e48ea0ee6e
Pikula <tomasz.pikula@accenture.com>
2025-02-19 17:42:46 +0100

    Refactor Hub arch

--- modules/virtualNetworkHub/main.tf
+++ modules/virtualNetworkHub/main.tf
@@ -3,23 +3,23 @@ module "hubnetworking_hub" {
 
   hub_virtual_networks = {
     for key, hub in var.hub_virtual_networks : key => {
-      name                            = hub.name
+      name                            = "${var.hub_name}-${hub.index}"
       address_space                   = hub.address_space
-      location                        = hub.location
+      location                        = var.location
       resource_group_name             = var.resource_group_name
       resource_group_creation_enabled = false
       resource_group_lock_enabled     = false
       mesh_peering_enabled            = true
-      route_table_name                = hub.route_table_name
+      route_table_name                = "${var.route_table_name}-${hub.index}"
       routing_address_space           = hub.routing_address_space
 
       firewall = hub.firewall != null ? {
         subnet_address_prefix = hub.firewall.subnet_address_prefix
-        name                  = hub.firewall.name
+        name                  = "${var.firewall_name}-${hub.index}"
         sku_name              = hub.firewall.sku_name
         sku_tier              = hub.firewall.sku_tier
         firewall_policy = {
-          name = hub.firewall.firewall_policy.name
+          name = "${var.firewall_policy_name}-${hub.index}"
           dns = {
             proxy_enabled = hub.firewall.firewall_policy.dns.proxy_enabled
           }
@@ -28,7 +28,7 @@ module "hubnetworking_hub" {
 
       subnets = {
         for subnet_key, subnet in hub.subnets : subnet_key => {
-          name             = subnet.name
+          name             = "${var.subnet_name}-${subnet.index}"
           address_prefixes = subnet.address_prefixes
           route_table      = subnet.route_table
         }
--- modules/virtualNetworkHub/variables.tf
+++ modules/virtualNetworkHub/variables.tf
@@ -1,18 +1,15 @@
 variable "hub_virtual_networks" {
   type = map(object({
-    name            = string
+    index            = string
     address_space   = list(string)
     location        = string
-    route_table_name = string
     routing_address_space = list(string)
 
     firewall = optional(object({
       subnet_address_prefix = string
-      name                  = string
       sku_name              = string
       sku_tier              = string
       firewall_policy = object({
-        name = string
         dns = object({
           proxy_enabled = bool
         })
@@ -20,7 +17,7 @@ variable "hub_virtual_networks" {
     }))
 
     subnets = map(object({
-      name             = string
+      index            = string
       address_prefixes = list(string)
       route_table      = optional(object({
         assign_generated_route_table = bool
@@ -31,5 +28,30 @@ variable "hub_virtual_networks" {
 
 variable "resource_group_name" {
   description = "The name of the resource group in which the virtual network will be created"
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "The name of the firewall"
+  type        = string
+}
+
+variable "firewall_policy_name" {
+  description = "The name of the firewall policy"
+  type        = string
+}
+
+variable "route_table_name" {
+  description = "The name of the route table"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+}
+
+variable "hub_name" {
+  description = "The name of the hub"
   type        = string
 }
