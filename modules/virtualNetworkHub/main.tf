fafebe27812b674cadbd6d9e34db077af65acd5b
Pikula <tomasz.pikula@accenture.com>
2025-02-26 14:58:47 +0100

    dinamicaly vnets - v4

--- modules/virtualNetworkHub/main.tf
+++ modules/virtualNetworkHub/main.tf
@@ -3,23 +3,23 @@ module "hubnetworking_hub" {
 
   hub_virtual_networks = {
     for key, hub in var.hub_virtual_networks : key => {
-      name                            = lookup(var.naming[hub.index].virtual_network, "name")
+      name                            = "${lookup(var.naming[hub.location].virtual_network, "name")}-${hub.index}"
       address_space                   = hub.address_space
-      location                        = lookup(var.resource_group_name[hub.index], "location")
-      resource_group_name             = lookup(var.resource_group_name[hub.index], "rg")
+      location                        = lookup(var.resource_group_name[hub.location], "location")
+      resource_group_name             = lookup(var.resource_group_name[hub.location], "rg")
       resource_group_creation_enabled = false
       resource_group_lock_enabled     = false
       mesh_peering_enabled            = true
-      route_table_name                = lookup(var.naming[hub.index].route_table, "name")
+      route_table_name                = "${lookup(var.naming[hub.location].route_table, "name")}-${hub.index}"
       routing_address_space           = hub.routing_address_space
 
       firewall = hub.firewall != null ? {
         subnet_address_prefix = hub.firewall.subnet_address_prefix
-        name                  = lookup(var.naming[hub.index].firewall, "name")
+        name                  = "${lookup(var.naming[hub.location].firewall, "name")}-${hub.index}"
         sku_name              = hub.firewall.sku_name
         sku_tier              = hub.firewall.sku_tier
         firewall_policy = {
-          name = lookup(var.naming[hub.index].firewall_policy, "name")
+          name = "${lookup(var.naming[hub.location].firewall_policy, "name")}-${hub.index}"
           dns = {
             proxy_enabled = hub.firewall.firewall_policy.dns.proxy_enabled
           }
@@ -28,7 +28,7 @@ module "hubnetworking_hub" {
 
       subnets = {
         for subnet_key, subnet in hub.subnets : subnet_key => {
-          name             = lookup(var.naming[subnet.index].subnet, "name")
+          name             = "${lookup(var.naming[hub.location].subnet, "name")}-${subnet.index}"
           address_prefixes = subnet.address_prefixes
           route_table      = subnet.route_table
         }
