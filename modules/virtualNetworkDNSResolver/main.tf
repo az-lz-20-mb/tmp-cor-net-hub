8373058532ecdda653f26f52845de6d8e6794792
Pikula <tomasz.pikula@accenture.com>
2025-03-13 18:39:48 +0100

    DNS resolver

---
+++ modules/virtualNetworkDNSResolver/main.tf
@@ -0,0 +1,35 @@
+module "private_resolver" {
+  source = "Azure/avm-res-network-dnsresolver/azurerm"
+  for_each                    = var.resolvers
+  resource_group_name         = lookup(var.resource_group_name[hub.location], "rg")
+  name                        = "resolver"
+  virtual_network_resource_id = lookup(var.remote_virtual_networks[each.key], "id")
+  location                    = lookup(var.resource_group_name[hub.location], "location")
+
+  inbound_endpoints = {
+    "inbound1" = {
+      name        = "inbound1"
+      subnet_name = lookup(var.remote_virtual_networks[each.key].subnets, "name")
+    }
+  }
+
+  outbound_endpoints = {
+    "outbound1" = {
+      name        = "outbound1"
+      subnet_name = lookup(var.remote_virtual_networks[each.key].subnets["subnet1"], "name")
+      forwarding_ruleset = {
+        for ruleset_key, ruleset in each.value.forwarding_ruleset : ruleset_key => {
+          name  = ruleset.name
+          rules = {
+            for rule_key, rule in ruleset.rules : rule_key => {
+              name                     = rule.name
+              domain_name              = rule.domain_name
+              state                    = rule.state
+              destination_ip_addresses = rule.destination_ip_addresses
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ modules/virtualNetworkDNSResolver/variables.tf
@@ -0,0 +1,37 @@
+variable "resolvers" {
+  description = "Map of resolvers"
+  type = map(object({
+    index        = string
+    location_key   =    string
+    forwarding_ruleset = map(object({
+      name  = string
+      rules = map(object({
+        name                     = string
+        domain_name              = string
+        state                    = string
+        destination_ip_addresses = list(string)
+      }))
+    }))
+  }))
+}
+
+variable "resource_group_name" {
+  type = map(object({
+    rg = string
+    location = string
+  }))
+}
+
+variable "naming" {
+  description = "Naming convention object"
+  type        = map(object({
+    virtual_network = map(string)
+    subnet          = map(string)
+    route_table     = map(string)
+    firewall        = map(string)
+    firewall_policy = map(string)
+  }))
+}
+
+variable "remote_virtual_networks" {
+}
